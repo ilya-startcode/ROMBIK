70d4cce86c1d09a2e3a6f6d4bdb86b6b04d36dd4
ilya <60235342+ilya-startcode@users.noreply.github.com>
2022-10-18 19:59:17 +0300

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-if [ "$#" -ne "1" ]; then
-	echo "ERROR: Invalid number of arguments"
-	echo "Syntax example: ./4.sh <your_int_num> "
-	exit 1
-fi
-
-int_regex='^[0-9]+$'
-if ! [[ $1 =~ $int_regex ]]; then
-	echo "ERROR: Argument must be integer"
-	echo "Syntax example: ./4.sh <your_int_num> "
-	exit 1
-fi
-
-
 max_diamond_size=$1
 
 #first part of the romb
